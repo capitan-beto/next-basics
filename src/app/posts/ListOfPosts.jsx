0c2b95b3a0a2f67cdafa5c3bae170b9ff61d3462
Otroletravaladna <crnana98@gmail.com>
2023-04-21 01:52:46 -0300

    Add hidratation to like btn

--- src/app/posts/ListOfPosts.jsx
+++ src/app/posts/ListOfPosts.jsx
@@ -1,3 +1,5 @@
+import { LikeButton } from "../components/LIkeButton";
+
 const fetchPost = () => {
     return fetch("https://jsonplaceholder.typicode.com/posts")
         .then(res => res.json());
@@ -6,15 +8,11 @@ const fetchPost = () => {
 export default async function ListOfPosts() {
     const posts = await fetchPost();
 
-    const handleClick = () => {
-        alert("ME GUSTA ESTE POST");
-    }
-
     return posts.slice(0, 5).map(post => (
            <article key={post.id}>
                <h1 style={{ color:"blue" }}>{ post.title }</h1>
                <p>{post.body}</p>
-               <button onClick={handleClick}>Me Gusta</button>
+               <LikeButton id={post.id}/>
            </article>
        )
     )
