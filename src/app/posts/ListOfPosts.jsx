6a1f0667462e87e2de7cf9d59fce7b8c1b1ab7e6
Otroletravaladna <crnana98@gmail.com>
2023-04-21 00:56:54 -0300

    Crete like btn file

--- src/app/posts/ListOfPosts.jsx
+++ src/app/posts/ListOfPosts.jsx
@@ -6,10 +6,15 @@ const fetchPost = () => {
 export default async function ListOfPosts() {
     const posts = await fetchPost();
 
+    const handleClick = () => {
+        alert("ME GUSTA ESTE POST");
+    }
+
     return posts.slice(0, 5).map(post => (
            <article key={post.id}>
                <h1 style={{ color:"blue" }}>{ post.title }</h1>
                <p>{post.body}</p>
+               <button onClick={handleClick}>Me Gusta</button>
            </article>
        )
     )
