a1ab8bc1ffa7856a77b2b2f191a180045719dc7a
Otroletravaladna <crnana98@gmail.com>
2023-04-21 00:29:33 -0300

    Set example how to fetch client side react

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,5 +1,26 @@
-export default function PostPage() {
-    return(
-        <h1>este es el Post Page</h1>
+import { useEffect, useState } from "react";
+
+export default function PostPage({ params }) {
+    const [posts, setPosts] = useState([]);
+
+    useEffect( async () => {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+            const data = await response.json();
+            setPosts(data);
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
+    return (
+       <section>
+        {posts.map(post => (
+            <article key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </article>
+        ))}
+       </section>
     )
 }
