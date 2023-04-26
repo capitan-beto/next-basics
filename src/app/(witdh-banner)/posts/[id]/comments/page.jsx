98244f6c8028fb1f0395dbf7cf2ae52f1d067b2f
Otroletravaladna <crnana98@gmail.com>
2023-04-25 23:33:31 -0300

    Add avatar using images

--- src/app/(witdh-banner)/posts/[id]/comments/page.jsx
+++ src/app/(witdh-banner)/posts/[id]/comments/page.jsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import Image from "next/image"
 
 const fetchComments = async (id) => {
-    await new Promise(res => setTimeout(res, 3000));
-    throw new Error("Error al cargar los comentarios");
+    await new Promise(res => setTimeout(res, 500));
+    // throw new Error("Error al cargar los comentarios");
 
-    // return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
-    //     next: {
-    //         revalidate: 60
-    //     }
-    // })
-    // .then(res => res.json());
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
+        next: {
+            revalidate: 60
+        }
+    })
+    .then(res => res.json());
 }
 
 export default async function Post({ params }){
@@ -20,6 +21,7 @@ export default async function Post({ params }){
         <ul style={{ fontSize: "15px", background: "#444", color: "#fff"}}>
             {comments.map(comment => {
                 return <li key={comment.id}>
+                    <Image width="50" height="50" src={`https://unavatar.io/${comment.email}.com`} alt="" />
                     <h2>{comment.name}</h2>
                     <p>{comment.body}</p>
                 </li>
